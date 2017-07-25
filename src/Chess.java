f73e36325c29c9b9e89d53b4d3dcbb49eac4150a
Pman <30382848+Automage@users.noreply.github.com>
2017-07-25 18:27:13 +0400

    Update Chess.java
    - Created two instances of the Player class (white and black)
    - Introduced a couple of variables to handle temporary storage of returned data from various methods
    - Line 18: Player class requires an array with all possible piece names to function 
    - Line 22: Testing out the move() method (it works!!)

--- src/Chess.java
+++ src/Chess.java
@@ -3,12 +3,26 @@ public class Chess {
 
 	public static void main(String[] args) {
 		
+		
 		Board board = new Board();
+		Player playerWhite = new Player("White");
+		Player playerBlack = new Player("Black");
+		String piece = "";
+		int[] xy = new int[2];
+	
 		
 		board.create();
 		board.update();
 		board.draw();
+		
+		Player.pieceNames = board.getPieceNames();
+		piece = playerWhite.askPiece();
+		xy = playerWhite.askSquare();
+		
+		board.move(piece, xy);
+		
+		
+		
 
-	}
-
+}
 }
