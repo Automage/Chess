2dae08ed6a6fbfc15912a82e798bc6b28db254ba
Pman <30382848+Automage@users.noreply.github.com>
2017-07-26 02:41:49 +0400

    Update Chess.java
    - Added a temporary loop to test both players input

--- src/Chess.java
+++ src/Chess.java
@@ -3,23 +3,58 @@ public class Chess {
 
 	public static void main(String[] args) {
 		
+		//Initialization
 		
 		Board board = new Board();
+		boolean kingDown = false;
+		boolean proceed = false;
+		//Creating Players
 		Player playerWhite = new Player("White");
 		Player playerBlack = new Player("Black");
+		//Temporary storage
 		String piece = "";
 		int[] xy = new int[2];
 	
-		
 		board.create();
 		board.update();
 		board.draw();
-		
 		Player.pieceNames = board.getPieceNames();
-		piece = playerWhite.askPiece();
-		xy = playerWhite.askSquare();
 		
-		board.move(piece, xy);
+		//GAME START
+		
+		while (kingDown == false) {
+			
+			//White's turn
+			while (proceed == false) {
+					
+				piece = playerWhite.askPiece();
+				xy = playerWhite.askSquare();
+				board.move(piece, xy);
+				proceed = board.getValidity();
+			}
+						
+			board.update();
+			board.draw();
+			proceed = false;
+			
+			//Black's turn
+			while (proceed == false) {
+				
+				piece = playerBlack.askPiece();
+				xy = playerBlack.askSquare();
+				board.move(piece, xy);
+				proceed = board.getValidity();
+			}
+			
+			board.update();
+			board.draw();
+			proceed = false;
+		}
+		
+		System.out.println("gg");
+		
+		
+		
 		
 		
 		
