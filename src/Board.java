0792d7411b9bf6317bae97d5e47f66f4277b0969
Pman <30382848+Automage@users.noreply.github.com>
2017-07-25 14:39:53 +0400

    Update Board.java
    - Converted individual piece objects into an array list (in order to help with with looping later on)
    - Introduced array squareOccupiedByName to store either the piece name or a placeholder ("   "). Unfortunately could not use squareOccupiedBy.getName(), as that would require creating a new Piece subclass solely for the purpose of a placeholder for the square. Using squareOccupiedBy.getName() when the piece is null will return an null pointer error.
    - Updated the draw() method with a basic board (that outputs the squareOccupiedByName string for each respective square)
    - Updated the update() method

--- src/Board.java
+++ src/Board.java
@@ -4,68 +4,107 @@ public class Board {
 	static int[][] boardCoordinates = new int[8][8];
 	static boolean[][] squareOccupied = new boolean[8][8];
 	static Piece[][] squareOccupiedBy = new Piece[8][8];
+	static String[][] squareOccupiedByName = new String[8][8];
 	
-	//Player White Pieces
-	Piece R1 = new Rook("R1 ",0,0);
-	Piece N1 = new Knight("N1 ",1,0);
-	Piece B1 = new Bishop("B1 ",2,0);
-	Piece K1 = new King("K1 ",3,0);
-	Piece Q1 = new Queen("Q1 ",4,0);
-	Piece B2 = new Bishop("B2 ",5,0);
-	Piece N2 = new Knight("N2 ",6,0);
-	Piece R2 = new Rook("R2 ",7,0);
-	Piece[] WhitePawns = new Pawn[8];
+	//Creating White Pieces
+	Piece[] whitePieces = new Piece[8];
+	Piece[] whitePawns = new Pawn[8];
 	
-	//Player Black Pieces
-	Piece R1_ = new Rook("R1'",0,7);
-	Piece N1_ = new Knight("N1'",1,7);
-	Piece B1_ = new Bishop("B1'",2,7);
-	Piece K1_ = new King("K1'",3,7);
-	Piece Q1_ = new Queen("Q1'",4,7);
-	Piece B2_ = new Bishop("B2'",5,7);
-	Piece N2_ = new Knight("N2'",6,7);
-	Piece R2_ = new Rook("R2'",7,7);
-	Piece[] BlackPawns = new Pawn[8];
+	//Creating Black Pieces 
+	Piece[] blackPieces = new Piece[8];
+	Piece[] blackPawns = new Pawn[8];
+	
 	
 	public void create() {
 		
-		//Player White Pawns
-		for (int i = 0;i<7;i++) {
-			WhitePawns[i] = new Pawn("P"+(i+1)+" ",i,1);
-			BlackPawns[i] = new Pawn("P"+(i+1)+"'",i,6);
+		//Initializing White Pieces
+		whitePieces[0] = new Rook("R1 ",0,0);
+		whitePieces[1] = new Knight("N1 ",1,0);
+		whitePieces[2] = new Bishop("B1 ",2,0);
+		whitePieces[3] = new King("K1 ",3,0);
+		whitePieces[4] = new Queen("Q1 ",4,0);
+		whitePieces[5] = new Bishop("B2 ",5,0); 
+		whitePieces[6] = new Knight("N2 ",6,0);
+		whitePieces[7] = new Rook("R2 ",7,0);
+		
+		//Initializing Black Pieces
+		blackPieces[0] = new Rook("R1'",0,7);
+		blackPieces[1] = new Knight("N1'",1,7);
+		blackPieces[2] = new Bishop("B1'",2,7);
+		blackPieces[3] = new King("K1'",3,7);
+		blackPieces[4] = new Queen("Q1'",4,7);
+		blackPieces[5] = new Bishop("B2'",5,7);
+		blackPieces[6] = new Knight("N2'",6,7);
+		blackPieces[7] = new Rook("R2'",7,7);
+		
+		//Pawns
+		for (int i = 0;i<8;i++) {
+			whitePawns[i] = new Pawn("P"+(i+1)+" ",i,1);
+			blackPawns[i] = new Pawn("P"+(i+1)+"'",i,6);
 		}
 		
 	}
 	
 	public void update() {
-		for (int y = 0;y<7;y++) {
-			for (int x = 0;x < 7;x++) {
-				squareOccupied[x][y] = false;
-				squareOccupiedBy[x][y] = null;
+		
+		String name = "";
+		int x,y = 0;
+		
+		//Nullify the whole board (removes the need to check every square for a piece)
+		// i is the x coordinate, j is the y coordinate
+		for (int i = 0;i<8;i++) {
+			for (int j = 0;j < 8;j++) {
+				squareOccupied[j][i] = false;
+				squareOccupiedBy[j][i] = null;
+				squareOccupiedByName[j][i] = "   ";
 			}
 		}
 		
+		for (int i = 0;i<8;i++){
+			
+			//White pieces
+			name = whitePieces[i].getName();
+			x = whitePieces[i].getX();
+			y = whitePieces[i].getY();
+			squareOccupied[x][y] = true;
+			squareOccupiedBy[x][y] = whitePieces[i];
+			squareOccupiedByName[x][y] = whitePieces[i].getName();
+			//White pawns
+			name = whitePawns[i].getName();
+			x = whitePawns[i].getX();
+			y = whitePawns[i].getY();
+			squareOccupied[x][y] = true;
+			squareOccupiedBy[x][y] = whitePawns[i];
+			squareOccupiedByName[x][y] = whitePawns[i].getName();
+			//Black pieces
+			name = blackPieces[i].getName();
+			x = blackPieces[i].getX();
+			y = blackPieces[i].getY();
+			squareOccupied[x][y] = true;
+			squareOccupiedBy[x][y] = blackPieces[i];
+			squareOccupiedByName[x][y] = blackPieces[i].getName();
+			//Black pawns
+			name = blackPawns[i].getName();
+			x = blackPawns[i].getX();
+			y = blackPawns[i].getY();
+			squareOccupied[x][y] = true;
+			squareOccupiedBy[x][y] = blackPawns[i];
+			squareOccupiedByName[x][y] = blackPawns[i].getName();
+		}
 		
 	}
 	
 	public void draw() {
+		
+		for (int y = 0; y<8;y++) {
+			System.out.println("---------------------------------------------------------");
+			for (int x = 0;x<8;x++) {
+				System.out.print("|  "+squareOccupiedByName[x][y]+" ");
+			}
+			System.out.print("|"+'\n');
+		}
 		System.out.println("---------------------------------------------------------");
-		System.out.println("|  C1' |  K1' |  B1' |  QN' |  KG' |  B2' |  K2' |  C2' |");
-		System.out.println("---------------------------------------------------------");
-		System.out.println("|  P1' |  P2' |  P3' |  P4' |  P5' |  P6' |  P7' |  P8' |");
-		System.out.println("---------------------------------------------------------");
-		System.out.println("|      |      |      |      |      |      |      |      |");
-		System.out.println("---------------------------------------------------------");
-		System.out.println("|      |      |      |      |      |      |      |      |");
-		System.out.println("---------------------------------------------------------");
-		System.out.println("|      |      |      |      |      |      |      |      |");
-		System.out.println("---------------------------------------------------------");
-		System.out.println("|      |      |      |      |      |      |      |      |");
-		System.out.println("---------------------------------------------------------");
-		System.out.println("|  P1  |  P2  |  P3  |  P4  |  P5  |  P6  |  P7  |  P8  |");
-		System.out.println("---------------------------------------------------------");
-		System.out.println("|  C1  |  K1  |  B1  |  QN  |  KG  |  B2  |  K2  |  C2  |");
-		System.out.println("---------------------------------------------------------");
+		
 	}
 	
 }
