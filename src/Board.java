b759d5134f29a9627fdab9db007960b91cbe7c5e
Pman <30382848+Automage@users.noreply.github.com>
2017-07-27 01:27:35 +0400

    Update Board.java
    - Updated the process() method. Now detects friendlies and opponents, and accordingly pushes an error or captures the opponent (i.e. sets the isDead boolean to true). Added a type parameter (Black or White) to determine friends and foes. Still does not detect obstacles (all pieces basically jump over all other pieces).
    - Process() method detects checkmates now, and accordingly sets the two new variables, kingDead and winner, to true and (Black or White)
    - Process() method now outputs turn summaries (movement and captures)
    - The Update() method now utilizes the isDead field from the Piece subclasses to determine whether to update it's position or just leave it null (Stops the draw() method from outputting captured pieces)
    - Create() method now specifies the isDead variable as false for all pieces (good practice right). Updated the parameters of the piece constructors (due to the addition of a type parameter in the Piece constructor)
    - General output cleanups (new lines, etc.)

--- src/Board.java
+++ src/Board.java
@@ -6,6 +6,9 @@ public class Board {
 	static Piece[][] squareOccupiedBy = new Piece[8][8];
 	static String[][] squareOccupiedByName = new String[8][8];
 	private static boolean moveValidity;
+	private static boolean kingDead;
+	private static String winner = "";
+	
 	
 	//Creating White Pieces
 	Piece[] whitePieces = new Piece[8];
@@ -19,29 +22,33 @@ public class Board {
 	public void create() {
 		
 		//Initializing White Pieces
-		whitePieces[0] = new Rook("R1 ",0,0);
-		whitePieces[1] = new Knight("N1 ",1,0);
-		whitePieces[2] = new Bishop("B1 ",2,0);
-		whitePieces[3] = new King("K1 ",3,0);
-		whitePieces[4] = new Queen("Q1 ",4,0);
-		whitePieces[5] = new Bishop("B2 ",5,0); 
-		whitePieces[6] = new Knight("N2 ",6,0);
-		whitePieces[7] = new Rook("R2 ",7,0);
+		whitePieces[0] = new Rook("R1 ",0,0,"White");
+		whitePieces[1] = new Knight("N1 ",1,0,"White");
+		whitePieces[2] = new Bishop("B1 ",2,0,"White");
+		whitePieces[3] = new King("K1 ",3,0,"White");
+		whitePieces[4] = new Queen("Q1 ",4,0,"White");
+		whitePieces[5] = new Bishop("B2 ",5,0,"White"); 
+		whitePieces[6] = new Knight("N2 ",6,0,"White");
+		whitePieces[7] = new Rook("R2 ",7,0,"White");
 		
 		//Initializing Black Pieces
-		blackPieces[0] = new Rook("R1'",0,7);
-		blackPieces[1] = new Knight("N1'",1,7);
-		blackPieces[2] = new Bishop("B1'",2,7);
-		blackPieces[3] = new King("K1'",3,7);
-		blackPieces[4] = new Queen("Q1'",4,7);
-		blackPieces[5] = new Bishop("B2'",5,7);
-		blackPieces[6] = new Knight("N2'",6,7);
-		blackPieces[7] = new Rook("R2'",7,7);
+		blackPieces[0] = new Rook("R1'",0,7,"Black");
+		blackPieces[1] = new Knight("N1'",1,7,"Black");
+		blackPieces[2] = new Bishop("B1'",2,7,"Black");
+		blackPieces[3] = new King("K1'",3,7,"Black");
+		blackPieces[4] = new Queen("Q1'",4,7,"Black");
+		blackPieces[5] = new Bishop("B2'",5,7,"Black");
+		blackPieces[6] = new Knight("N2'",6,7,"Black");
+		blackPieces[7] = new Rook("R2'",7,7,"Black");
 		
 		//Pawns
 		for (int i = 0;i<8;i++) {
-			whitePawns[i] = new Pawn("P"+(i+1)+" ",i,1);
-			blackPawns[i] = new Pawn("P"+(i+1)+"'",i,6);
+			whitePieces[i].setDead(false);
+			blackPieces[i].setDead(false);
+			whitePawns[i] = new Pawn("P"+(i+1)+" ",i,1,"White");
+			whitePawns[i].setDead(false);
+			blackPawns[i] = new Pawn("P"+(i+1)+"'",i,6,"Black");
+			blackPawns[i].setDead(false);
 		}
 		
 	}
@@ -63,35 +70,44 @@ public class Board {
 		for (int i = 0;i<8;i++){
 			
 			//White pieces
-			x = whitePieces[i].getX();
-			y = whitePieces[i].getY();
-			squareOccupied[x][y] = true;
-			squareOccupiedBy[x][y] = whitePieces[i];
-			squareOccupiedByName[x][y] = whitePieces[i].getName();
+			if (whitePieces[i].getDead() == false) {
+				x = whitePieces[i].getX();
+				y = whitePieces[i].getY();
+				squareOccupied[x][y] = true;
+				squareOccupiedBy[x][y] = whitePieces[i];
+				squareOccupiedByName[x][y] = whitePieces[i].getName();
+			}
 			//White pawns
-			x = whitePawns[i].getX();
-			y = whitePawns[i].getY();
-			squareOccupied[x][y] = true;
-			squareOccupiedBy[x][y] = whitePawns[i];
-			squareOccupiedByName[x][y] = whitePawns[i].getName();
+			if (whitePawns[i].getDead() == false) {
+				x = whitePawns[i].getX();
+				y = whitePawns[i].getY();
+				squareOccupied[x][y] = true;
+				squareOccupiedBy[x][y] = whitePawns[i];
+				squareOccupiedByName[x][y] = whitePawns[i].getName();
+			}
 			//Black pieces
-			x = blackPieces[i].getX();
-			y = blackPieces[i].getY();
-			squareOccupied[x][y] = true;
-			squareOccupiedBy[x][y] = blackPieces[i];
-			squareOccupiedByName[x][y] = blackPieces[i].getName();
+			if (blackPieces[i].getDead() == false) {
+				x = blackPieces[i].getX();
+				y = blackPieces[i].getY();
+				squareOccupied[x][y] = true;
+				squareOccupiedBy[x][y] = blackPieces[i];
+				squareOccupiedByName[x][y] = blackPieces[i].getName();
+			}			
 			//Black pawns
-			x = blackPawns[i].getX();
-			y = blackPawns[i].getY();
-			squareOccupied[x][y] = true;
-			squareOccupiedBy[x][y] = blackPawns[i];
-			squareOccupiedByName[x][y] = blackPawns[i].getName();
+			if (blackPawns[i].getDead() == false) {
+				x = blackPawns[i].getX();
+				y = blackPawns[i].getY();
+				squareOccupied[x][y] = true;
+				squareOccupiedBy[x][y] = blackPawns[i];
+				squareOccupiedByName[x][y] = blackPawns[i].getName();
+			}		
 		}
 		
 	}
 	
 	public void draw() {
-
+		
+		System.out.println();
 		for (int y = 7; y>=0;y--) {
 			System.out.println("---------------------------------------------------------");
 			for (int x = 0;x<8;x++) {
@@ -113,69 +129,65 @@ public class Board {
 		while (check == false) {
 			if (piece.equals(whitePieces[i].getName())) {
 				check = true;
-				isValid = process(whitePieces[i], xy, whitePieces[i].getPossibleSquares(whitePieces[i].getX(), whitePieces[i].getY(), "White"));
+				isValid = process(whitePieces[i], xy, whitePieces[i].getPossibleSquares(whitePieces[i].getX(), whitePieces[i].getY(), "White"), whitePieces[i].getType());
 				if (isValid == true) {
 					whitePieces[i].setX(xy[0]);
 					whitePieces[i].setY(xy[1]);
 				}
 				else {
-					System.out.println("Invalid move");
+					System.out.println("Invalid move\n");
 					return;
 				}
 			} 
 			else if (piece.equals(whitePawns[i].getName())) {
 				check = true;
-				isValid = process(whitePawns[i], xy, whitePawns[i].getPossibleSquares(whitePawns[i].getX(), whitePawns[i].getY(), "White"));
+				isValid = process(whitePawns[i], xy, whitePawns[i].getPossibleSquares(whitePawns[i].getX(), whitePawns[i].getY(), "White"), whitePawns[i].getType());
 				if (isValid == true) {
 					whitePawns[i].setX(xy[0]);
 					whitePawns[i].setY(xy[1]);
 				}
 				else {
-					System.out.println("Invalid move");
+					System.out.println("Invalid move\n");
 					return;
 				}
 			}
 			else if (piece.equals(blackPieces[i].getName())) {
 				check = true;
-				isValid = process(blackPieces[i], xy, blackPieces[i].getPossibleSquares(blackPieces[i].getX(), blackPieces[i].getY(), "Black"));
+				isValid = process(blackPieces[i], xy, blackPieces[i].getPossibleSquares(blackPieces[i].getX(), blackPieces[i].getY(), "Black"), blackPieces[i].getType());
 				if (isValid == true) {
 					blackPieces[i].setX(xy[0]);
 					blackPieces[i].setY(xy[1]);
 				}
 				else {
-					System.out.println("Invalid move");
+					System.out.println("Invalid move\n");
 					return;
 				}
 			}
 			else if (piece.equals(blackPawns[i].getName())){
 				check = true;
-				isValid = process(blackPawns[i], xy, blackPawns[i].getPossibleSquares(blackPawns[i].getX(), blackPawns[i].getY(), "Black"));
+				isValid = process(blackPawns[i], xy, blackPawns[i].getPossibleSquares(blackPawns[i].getX(), blackPawns[i].getY(), "Black"), blackPawns[i].getType());
 				if (isValid == true) {
 					blackPawns[i].setX(xy[0]);
 					blackPawns[i].setY(xy[1]);
 				}
 				else {
-					System.out.println("Invalid move");
+					System.out.println("Invalid move\n");
 					return;
 				}
 			}
 			i++;
 		}
 		
+
 		
 	}
 	
-	Boolean process(Piece piece, int[] xy, String[] possibleSquares) {
+	Boolean process(Piece piece, int[] xy, String[] possibleSquares, String type) {
 		
 		moveValidity = false;
 		boolean check = false;
-		int i = 0;
-		
-		//Monitoring purposes
-		for (int a = 0; a<possibleSquares.length;a++) {
-			System.out.println(possibleSquares[a]);
-		}
-		
+		int i = 0;		
+
 		//Checks whether the players square choice matches a possible square
 		while ((check == false) && (i<possibleSquares.length)) {
 			if ((Board.getSquareString(xy[0], xy[1])).equals(possibleSquares[i])) {
@@ -185,13 +197,39 @@ public class Board {
 				i++;
 			}
 		}
+		System.out.println();
 		
 		if (check == false) {
 			moveValidity = false;
 		}
 		else {
-			//rest of the checking
-			moveValidity = true; 
+			
+			//Checking for occupancy of input square
+			if (squareOccupied[xy[0]][xy[1]] == true) {
+				
+				//Check whether the foreign piece is in the same team or not
+				if (squareOccupiedBy[xy[0]][xy[1]].getType().equals(piece.getType())) {
+					
+					//same team
+					moveValidity = false;
+				}
+				else { //opponent capture
+					moveValidity = true;
+					squareOccupiedBy[xy[0]][xy[1]].setDead(true);
+					System.out.println(piece.getName()+" to "+Board.getSquareString(xy[0], xy[1]));
+					System.out.println(squareOccupiedBy[xy[0]][xy[1]].getName()+" captured!");
+					//Check for check-mate
+					if ((squareOccupiedBy[xy[0]][xy[1]].getClass().toString()).equals("class King")) {
+						kingDead = true;
+						setWinner(piece.getType());
+					}
+				}
+			}
+			else {
+				//Empty square
+				moveValidity = true;
+				System.out.println(piece.getName()+" to "+Board.getSquareString(xy[0], xy[1]));
+			} 
 		}
 		
 		return moveValidity;
@@ -208,6 +246,18 @@ public class Board {
 		return moveValidity;
 	}
 	
+	public static boolean getKingDead() {
+		return kingDead;
+	}
+
+	public static String getWinner() {
+		return winner;
+	}
+
+	public static void setWinner(String winner) {
+		Board.winner = winner;
+	}
+
 	public String[] getPieceNames() {
 
 		
