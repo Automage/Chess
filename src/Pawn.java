0838c51e2ae3b4ee08fe542d61e3d0be68450e44
Pman <30382848+Automage@users.noreply.github.com>
2017-07-26 13:56:17 +0400

    Override getPossibleSquares()
    - Override of the getPossibleSquares() method
    - Since the movement of the pawn is dependent on the type of player (Black or White), the parameter of type(String) was introduced in order to distinguish
    - The method also takes into account the pawn's unique capturing ability (ability to capture diagonally only), therefore producing 3 possible squares at max (the process() method will take care of the unique mechanics of the pawn [in 1.6 hopefully])

--- src/Pawn.java
+++ src/Pawn.java
@@ -1,8 +1,81 @@
+import java.util.*;
 
 public class Pawn extends Piece{
 
 	public Pawn(String name, int x, int y) {
 		super(name,x,y);
+	}
+
+	@Override
+	public String[] getPossibleSquares(int x, int y, String type) {
+		
+		ArrayList<String> possibleSquares = new ArrayList<String>();
+		//Clone to modify allows the reference xy to stay constant
+		int xMod = x;
+		int yMod = y;
+		
+		if (type.equals("White")) { //For white pawns
+			//Checks if pawn is on the 8th y row
+			if (y<7) {
+				yMod = yMod+1;
+				possibleSquares.add(Board.getSquareString(xMod,yMod));
+				
+				//Checks if the pawn can capture diagonal left by checking if it is on the A x column
+				if (x>0) {
+					xMod = x;
+					yMod = y;
+					
+					xMod = xMod - 1;
+					yMod = yMod + 1;
+					possibleSquares.add(Board.getSquareString(xMod,yMod));
+				}
+				
+				//Checks if the pawn can capture diagonal right by checking if it is on the H x column
+				if (x<7) {
+					xMod = x;
+					yMod = y;
+					
+					xMod = xMod + 1;
+					yMod = yMod + 1;
+					possibleSquares.add(Board.getSquareString(xMod,yMod));
+				}
+			}
+			
+		}
+		else if (type.equals("Black")) { //For black pawns
+			
+			//Checks if pawn is on the 1st y row
+			if (y>0) {
+				yMod = yMod - 1;
+				possibleSquares.add(Board.getSquareString(xMod,yMod));
+				
+				//Checks if the pawn can capture diagonal left by checking if it is on the A x column
+				if (x>0) {
+					xMod = x;
+					yMod = y;
+					
+					xMod = xMod - 1;
+					yMod = yMod - 1;
+					possibleSquares.add(Board.getSquareString(xMod,yMod));
+				}
+				
+				//Checks if the pawn can capture diagonal right by checking if it is on the H x column
+				if (x<7) {
+					xMod = x;
+					yMod = y;
+					
+					xMod = xMod + 1;
+					yMod = yMod - 1;
+					possibleSquares.add(Board.getSquareString(xMod,yMod));
+				}
+			}
+		}
+		
+		String[] possibleSquaresArray = new String[possibleSquares.size()];
+		for (int i = 0;i<possibleSquares.size();i++) {
+			possibleSquaresArray[i] = possibleSquares.get(i);
+		}
+		return possibleSquaresArray;
 	}
 
 }
