e94d60930fb91f8a4e128962b388c55ccaef3ebc
Pman <30382848+Automage@users.noreply.github.com>
2017-08-14 18:47:55 +0400

    Update Queen.java
    - canJump property updated (false)

--- src/Queen.java
+++ src/Queen.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Queen extends Piece{
 
 	public Queen(String name, int x, int y, String type) {
-		super(name,x,y,type);
+		super(name,x,y,type,false);
 	}
 
 	@Override
